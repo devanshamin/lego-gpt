c87554c20587183eb593b746fd32d4a8c3812797
damin604 <DAmin@bioxceltherapeutics.com>
2024-03-06 15:37:07 -0500

    Add `SwiGLU` information to the docstring

--- lego_gpt/blocks/transformer/feedforward.py
+++ lego_gpt/blocks/transformer/feedforward.py
@@ -4,6 +4,12 @@ import torch.nn.functional as F
 
 
 class FeedForward(nn.Module):
+    """Feed Forward Network (FNN) [SwiGLU](https://arxiv.org/pdf/2002.05202.pdf) layer.
+    
+    `FFNSwiGLU (x, W, V, W2) = (Swi(xW) ⊙ xV) W2`
+    * Swi - [Swish](https://arxiv.org/abs/1710.05941v1) activation function
+    * GLU - GLU (Gated Linear Units) is a neural network layer
+    """
 
     def __init__(self, n_embd: int, intermediate_size: int):
         
@@ -14,4 +20,8 @@ class FeedForward(nn.Module):
 
     def forward(self, x: torch.FloatTensor):
         
+        # `F.silu` - acts as gating mechanism that controls the flow of information
+        #            from the linear transformation
+        # `F.silu(self.w1(x)) * self.w3(x)` - GLU; linear transformation followed 
+        #            by a gating mechanism
         return self.w2(F.silu(self.w1(x)) * self.w3(x))
