f4f19aa7d093cc5304d9304be6bfe722ac4a378a
damin604 <DAmin@bioxceltherapeutics.com>
2024-03-06 22:08:14 -0500

    Add `ignore_patterns` argument to ignore model artifacts

--- scripts/hf_model_downloader.py
+++ scripts/hf_model_downloader.py
@@ -17,9 +17,24 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--repo_id", type=str, required=True, help="HuggingFace repository ID.")
     parser.add_argument("--cache_dir", type=str, default=".cache", help="Cache directory for downloading and saving the HuggingFace models.")
+    parser.add_argument(
+        "--ignore_patterns", 
+        type=str, 
+        default="", 
+        help=(
+            "If provided, files matching any of the patterns are not downloaded. "
+            "Multiple patterns should be comma separated (i.e., stablelm-*,*.gguf)"
+        )
+    )
 
     args = parser.parse_args()
-
     cache_dir = Path.cwd() / args.cache_dir
     cache_dir.mkdir(exist_ok=True)
-    snapshot_download(repo_id=args.repo_id, cache_dir=cache_dir, token=HF_ACCESS_TOKEN)
+    if ignore_patterns := args.ignore_patterns:
+        ignore_patterns = args.ignore_patterns.split(",")
+    snapshot_download(
+        repo_id=args.repo_id, 
+        cache_dir=cache_dir, 
+        token=HF_ACCESS_TOKEN,
+        ignore_patterns=ignore_patterns
+    )
